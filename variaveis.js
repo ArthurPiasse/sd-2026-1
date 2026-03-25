225e2a8e0a499770d0eba0fc1bc95b910f0e7263
Arthur Piasse <piassearthur2021@gmail.com>
2026-03-24 22:21:41 -0300

    aula-24-03-variaveis

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+/* - Variáveis em JavaScript são Case-Sensitive nome e NOME são variáveis diferentes
+- Variáveis em JavaScript tem suport a UNICODE, ou seja, podem conter caracteres acentuados e emojis */
+
+// nome_completo snake_case
+// nomeCompleto     camelCase   utilizado para variáveis e funções em JavaScript
+// NomeCompleto     PascalCase
+
+//Declaração de variáveis
+
+var nome = "João"
+let sobrenome = "Silva"
+const idade = 30
+
+if (true) {
+    var nome = "maria"
+    let sobrenome = "souza"
+}
+
+console.log(nome)
+console.log(sobrenome)
+console.log(idade)
